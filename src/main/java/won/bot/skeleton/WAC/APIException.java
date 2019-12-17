1db1b91fe509a987a3a367e3e52de3acb081c6a1
Fefe96 <46799330+Fefe96@users.noreply.github.com>
2019-12-17 14:16:24 +0100

    Update APIException.java

--- src/main/java/won/bot/skeleton/WAC/APIException.java
+++ src/main/java/won/bot/skeleton/WAC/APIException.java
@@ -1,4 +1,5 @@
 //HOWTO https://alvinalexander.com/java/java-custom-exception-create-throw-exception
+package won.bot.skeleton.WAC;
 
 public class APIException extends Exception{
     APIException(String message){
