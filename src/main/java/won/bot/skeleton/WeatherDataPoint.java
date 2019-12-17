a47ffbc5d44ba090ffd83ca4d941f417e02d89c3
Fefe96 <46799330+Fefe96@users.noreply.github.com>
2019-12-17 14:11:09 +0100

    Update WeatherDataPoint.java

--- src/main/java/won/bot/skeleton/WeatherDataPoint.java
+++ src/main/java/won/bot/skeleton/WeatherDataPoint.java
@@ -1,13 +1,22 @@
 package won.bot.skeleton;
 
+import won.bot.skeleton.WAC.Weather;
+
 public class WeatherDataPoint {
     private float highestTemperature;
+    private Weather weather;
 
-    public WeatherDataPoint(float highestTemperature) {
+    /*public WeatherDataPoint(float highestTemperature) {
         this.highestTemperature = highestTemperature;
+    }*/
+    public WeatherDataPoint(Weather weather) {
+        this.weather = weather;
     }
 
-    public float getHighestTemperature() {
+    /*public float getHighestTemperature() {
         return highestTemperature;
+    }*/
+    public Weather getWeather() {
+        return weather;
     }
 }
